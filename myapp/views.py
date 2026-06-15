ab8af77e04faeaaf3144c4ac5d26b27ff1be8f9b
Amcelo13 <tchetan308@gmail.com>
2026-06-14 23:43:22 -0400

    feat: implement Django template-based rendering with base layouts and CSS styling for myapp views

--- myapp/views.py
+++ myapp/views.py
@@ -1,49 +1,26 @@
 # Import necessary classes
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Publisher, Book, Member, Order
 
 # Create your views here.
 
 
 def index(request):
-    """Main landing page - shows list of books and publishers"""
-    response = HttpResponse()
-
-    # Display list of books ordered by primary key
-    heading1 = '<p>' + 'List of available books: ' + '</p>'
-    response.write(heading1)
+    """Main landing page - shows list of books"""
     booklist = Book.objects.all().order_by('id')[:10]
-    for book in booklist:
-        para = '<p>' + str(book.id) + ': ' + str(book) + '</p>'
-        response.write(para)
-
-    # Display list of Publishers sorted by city (descending)
-    heading2 = '<p>' + 'List of Publishers: ' + '</p>'
-    response.write(heading2)
-    publisherlist = Publisher.objects.all().order_by('-city')
-    for publisher in publisherlist:
-        para = '<p>' + publisher.name + ' - ' + publisher.city + '</p>'
-        response.write(para)
-
-    return response
+    return render(request, 'myapp/index.html', {'booklist': booklist})
+    # Passing context variable: booklist (list of books)
 
 
 def about(request):
     """About page for the eBook APP"""
-    return HttpResponse("This is an eBook APP.")
+    return render(request, 'myapp/about.html')
+    # Passing context variables: NO - no extra context variables needed
 
 
 def detail(request, book_id):
     """Detail page for a specific book"""
     book = get_object_or_404(Book, pk=book_id)
-
-    response = HttpResponse()
-    para = '<p>' + 'Title: ' + book.title.upper() + '</p>'
-    response.write(para)
-    para = '<p>' + 'Price: $' + str(book.price) + '</p>'
-    response.write(para)
-    para = '<p>' + 'Publisher: ' + str(book.publisher) + '</p>'
-    response.write(para)
-
-    return response
+    return render(request, 'myapp/detail.html', {'book': book})
+    # Passing context variable: book (the specific book object)
